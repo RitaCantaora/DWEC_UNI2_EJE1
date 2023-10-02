34dc3e61473d8165f7ac8d665617fdec8b0649d0
mdegs02 <mdegs02@educastur.es>
2023-10-02 13:46:32 +0200

    ejercicio1

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,9 +1,11 @@
 
+
 // Pedir al usuario su nombre y apellidos
 const nombreCompleto = prompt("Por favor, ingresa tu nombre y apellidos:");
 
 // Tamaño del nombre y apellidos (sin contar espacios)
-const longitudNombre = nombreCompleto.replace(/\s/g, "").length;
+console.log("El tamaño del nombre y apellidos sin espacios es de: " 
++ entrada.replaceAll(" ","").length); 
 
 // Convertir a minúsculas y mayúsculas
 const nombreMinusculas = nombreCompleto.toLowerCase();
@@ -37,3 +39,52 @@ for (let i = 0; i < nombre.length; i++) {
     contraseña += nombre.charCodeAt(i) - 96; // Restamos 96 para obtener la posición en el alfabeto
 }
 console.log("Contraseña: " + contraseña);
+
+// SOLUCIÓN PROFESOR
+entrada = prompt("Introduce tu nombre y apellidos");
+console.log("El nombre introducido es: " + entrada);
+
+// El tamaño del nombre más los apellidos (sin contar espacios)
+console.log("El tamaño del nombre y apellidos sin espacios es de: " 
+        + entrada.split(" ").join("").length);
+         
+//también valdría un replaceAll
+console.log("El tamaño del nombre y apellidos sin espacios es de: " 
+        + entrada.replaceAll(" ","").length); 
+
+// La cadena en minúsculas y en mayúsculas
+console.log("Minus: " + entrada.toLowerCase());
+console.log("Mayus: " + entrada.toUpperCase());
+
+/* Que divida el nombre y los apellidos y los muestr en 3 líneas
+ * donde ponga "Nombre: /nApellido 1:/nApellido2:"
+ */
+ var elementosEntrada = entrada.split(" ");
+ console.warn(elementosEntrada);
+ console.log("Nombre: " + elementosEntrada[0] +
+         "\nApellido 1: " + elementosEntrada[1] + 
+         "\nApellido 2: " + elementosEntrada[2]);
+
+// Propuesta de nombre de usuario -> davidfg
+console.log("Propuesta de nombre 2: " +
+elementosEntrada[0].toLowerCase() +
+(elementosEntrada[1])[0].toLowerCase() + 
+(elementosEntrada[2])[0].substring(0,1).toLowerCase()); 
+
+// Propuesta de nombre de usuario -> dafega
+console.log("Propuesta de nombre 3: " +
+(elementosEntrada[0]).substr(0,2).toLowerCase() +
+(elementosEntrada[1]).substr(0,2).toLowerCase() + 
+(elementosEntrada[2]).substr(0,2).toLowerCase()); 
+
+// Propuesta de contrasena
+
+function creaPassword(nombre){
+var resultado="";
+for (let i=0;i<nombre.length; i++){
+        resultado+=nombre.charAt(i)+i;
+}
+return resultado;
+}
+
+console.log("Propuesta de contraseña"+creaPassword(elementosEntrada[0]));
